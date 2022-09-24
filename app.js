b10a3473749df5434e826957d275f3d2e493f318
pranabacharya <pacharya808@gmail.com>
2022-09-24 19:48:50 +0530

    updated item array
    updated the item array and added the inr icon

--- app.js
+++ app.js
@@ -5,84 +5,84 @@ const shopItemData = [
         itemName: "Casual Shirts",
         itemDesc: "Pack of 3 casual shirts ",
         itemUrl: "img/shirt2.jpg",
-        itemPrice: 23
+        itemPrice: 399
     },
     {   
         id: "dfdf",
         itemName: "Stripped Half Shirt",
         itemDesc: "Navy blue color stripped half shirt ",
         itemUrl: "img/shirt3.png",
-        itemPrice: 62
+        itemPrice: 549
     },
     {   
         id: "afdsf",
         itemName: "Casual Partywear Shirt",
         itemDesc: "Pitch color shirt ",
         itemUrl: "img/shirt4.png",
-        itemPrice: 85
+        itemPrice: 859
     },
     {   
         id: "atge",
         itemName: "Black Half Shirt",
         itemDesc: "Black coloured half shirt ",
         itemUrl: "img/shirt5.png",
-        itemPrice: 55
+        itemPrice: 555
     },
     {   
         id: "fadsf",
         itemName: "T-Shirt Red",
         itemDesc: "Red polo t-shirt ",
         itemUrl: "img/tshirt6.png",
-        itemPrice: 36
+        itemPrice: 369
     },
     {   
         id: "bgae",
         itemName: "Casual Shirts",
         itemDesc: "Pack of 2 casual shirts ",
         itemUrl: "img/shirt7.png",
-        itemPrice: 125
+        itemPrice: 1250
     },
     {   
         id: "agaee",
         itemName: "Blue Stripped Full Formal",
         itemDesc: "Navy blue coloured stripped check shirt ",
         itemUrl: "img/shirt8.png",
-        itemPrice: 99
+        itemPrice: 999
     },
     {   
         id: "aghra",
         itemName: "Men Casual Shirt Full",
         itemDesc: "Men checked bordered blue full shirt ",
         itemUrl: "img/shirt9.png",
-        itemPrice: 69
+        itemPrice: 699
     },
     {   
         id: "aega",
         itemName: "Men Formal Shirt",
         itemDesc: "Sky color men formal shirt and slim fit ",
         itemUrl: "img/shirt10.png",
-        itemPrice: 55
+        itemPrice: 559
     },
     {   
         id: "agda",
         itemName: "Partywear Men Blue Shirt",
         itemDesc: "Navy Blue coloured partywear slim fit shirt",
         itemUrl: "img/shirt11.png",
-        itemPrice: 74
+        itemPrice: 748
     },
     {   
         id: "awetg",
         itemName: "Men Black Suit",
         itemDesc: "Men Black Suit with great comfort ",
         itemUrl: "img/suit12.png",
-        itemPrice: 189
+        itemPrice: 1892
     },
     {   
         id: "aegeg",
         itemName: "Pack of 4 Tees",
         itemDesc: "Pack of 4 tees ",
         itemUrl: "img/shirts.jpg",
-        itemPrice: 74
+        itemPrice: 749
     }
 ]
 const basket = [];
@@ -96,7 +96,7 @@ const generateItem = () => {
                     <h3>${itemName}</h3>
                     <p>${itemDesc}</p>
                     <div class="price-quantity">
-                        <h2>$${itemPrice}</h2>
+                        <h2><i class="fa-sharp fa-solid fa-indian-rupee-sign"></i> ${itemPrice}</h2>
                         <div class="buttons">
                             <i class="fa-solid fa-minus" onclick="decrement(${id})"></i>
                             <div class="quantity" id="${id}">0</div>
@@ -113,7 +113,7 @@ generateItem();
 
 const increment = (id) => {
     let selectedItem = id;
-    let search = basket.find(x=> x.id === selectedItem.id);
+    let search = basket.find(x => x.id === selectedItem.id);
 
     if(search === undefined){
         basket.push({
